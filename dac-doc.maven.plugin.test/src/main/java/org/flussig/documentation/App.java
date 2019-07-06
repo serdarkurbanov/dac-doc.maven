271bd2cdf5f256c571581128b386998e9d03fd25
Serdar Kurbanov <serdar.kurbanov@hotmail.com>
2019-07-05 22:08:09 -0400

    add submodules: dac-doc.maven.plugin & dac-doc.maven.plugin.test

---
+++ dac-doc.maven.plugin.test/src/main/java/org/flussig/documentation/App.java
@@ -0,0 +1,13 @@
+package org.flussig.documentation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
