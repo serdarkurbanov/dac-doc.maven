b1faf191d96e10d5d64cebffb048b18344d0807a
Serdar Kurbanov <serdar.kurbanov@hotmail.com>
2019-07-08 08:23:29 -0400

    leave parent anchor class only

--- dacdoc-core/src/main/java/org/flussig/documentation/text/DacDocAnchor.java
+++ dacdoc-core/src/main/java/org/flussig/documentation/text/DacDocAnchor.java
@@ -12,16 +12,17 @@ import java.util.stream.Collectors;
 /**
  * Class contains information of DACDOC placeholder and associated tags
  */
-public abstract class DacDocAnchor {
+public final class DacDocAnchor {
     protected String fullText;
     protected String id;
+    private Collection<String> ids = new ArrayList<>();
+    private String argument;
+    private String testId;
 
     /**
      * Generate anchor instance from full string
      */
     public static DacDocAnchor from(String fullText) throws DacDocException {
-        DacDocAnchor result = null;
-
         // format of full text of anchor is !DACDOC{...}(...)! for primitive type or !DACDOC(...)! for composite type
         String fullTextStripFraming = fullText.replaceAll(
                 String.format(
@@ -47,53 +48,41 @@ public abstract class DacDocAnchor {
         Map<String, String> paramMap = extractParameterMap(contentParameterTuple);
 
         // content present -> primitive anchor type; if not -> complex type
-        if(Strings.isNullOrEmpty(contentParameterTuple.content)) {
-            result = getComplexAnchor(paramMap);
-        } else {
-            result = getPrimitiveAnchor(contentParameterTuple.content, paramMap);
+        DacDocAnchor result = getAnchor(contentParameterTuple.content, paramMap);
 
-        }
-
-        // extract or generate ID
         result.fullText = fullText;
-        attachID(result, paramMap);
 
         return result;
     }
 
-    private static DacDocAnchor getPrimitiveAnchor(String content, Map<String, String> paramMap) {
-        DacDocAnchor result;
-        String testId;
+    private static DacDocAnchor getAnchor(String content, Map<String, String> paramMap) {
+        DacDocAnchor result = new DacDocAnchor();
 
+        // attach argument
+        result.argument = content;
+
+        // attach id of the test
         if(paramMap != null && !paramMap.isEmpty() && paramMap.containsKey(Constants.ANCHOR_PARAMETER_TEST_ID)) {
-            testId = paramMap.get(Constants.ANCHOR_PARAMETER_TEST_ID);
+            result.testId = paramMap.get(Constants.ANCHOR_PARAMETER_TEST_ID);
         } else {
-            testId = Constants.DEFAULT_TEST_ID;
+            result.testId = Constants.DEFAULT_TEST_ID;
         }
 
-        result = new DacDocPrimitiveAnchor(content, testId);
-        return result;
-    }
-
-    private static DacDocAnchor getComplexAnchor(Map<String, String> paramMap) {
-        DacDocAnchor result;
-        Collection<String> ids = new ArrayList<>();
+        // attach ids of child anchors
         if(paramMap != null && !paramMap.isEmpty() && paramMap.containsKey(Constants.ANCHOR_PARAMETER_IDS)) {
-            ids = Arrays.stream(paramMap.get(Constants.ANCHOR_PARAMETER_IDS).split(Constants.ANCHOR_PARAMETER_IDS_SEPARATOR))
+            result.ids = Arrays.stream(paramMap.get(Constants.ANCHOR_PARAMETER_IDS).split(Constants.ANCHOR_PARAMETER_IDS_SEPARATOR))
                     .map(String::trim)
                     .collect(Collectors.toList());
         }
 
-        result = new DacDocComplexAnchor(ids);
-        return result;
-    }
-
-    private static void attachID(DacDocAnchor result, Map<String, String> paramMap) {
+        // extract or generate ID
         if(paramMap != null && !paramMap.isEmpty() && paramMap.containsKey(Constants.ANCHOR_PARAMETER_ID)) {
             result.id =  paramMap.get(Constants.ANCHOR_PARAMETER_ID);
         } else {
             result.id = UUID.randomUUID().toString();
         }
+
+        return result;
     }
 
     private static Map<String, String> extractParameterMap(ContentParameterTuple contentParameterTuple) {
@@ -109,7 +98,39 @@ public abstract class DacDocAnchor {
     /**
      * Check anchor for internal consistency
      */
-    public abstract DacDocValidationResult validate();
+    public DacDocValidationResult validate() {
+        return new DacDocValidationResult();
+    }
+
+    public String getFullText() {
+        return fullText;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Collection<String> getIds() {
+        return ids;
+    }
+
+    public String getArgument() {
+        return argument;
+    }
+
+    public String getTestId() {
+        return testId;
+    }
+
+    public DacDocAnchorType getAnchorType() {
+        if(ids.isEmpty()) {
+            return DacDocAnchorType.PRIMITIVE;
+        } else {
+            return DacDocAnchorType.COMPOSITE;
+        }
+    }
+
+    private DacDocAnchor() {}
 
     /**
      * Tuple of content string and parameter string of a anchor
