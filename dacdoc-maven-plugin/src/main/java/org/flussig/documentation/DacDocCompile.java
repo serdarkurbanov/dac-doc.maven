357aacacf217d6f48d69812a3c78b4bbf944c0a5
Serdar Kurbanov <serdar.kurbanov@hotmail.com>
2019-07-09 19:53:23 -0400

    plugin: update logic of creation of dacdoc resources

--- dacdoc-maven-plugin/src/main/java/org/flussig/documentation/DacDocCompile.java
+++ dacdoc-maven-plugin/src/main/java/org/flussig/documentation/DacDocCompile.java
@@ -24,11 +24,12 @@ import org.flussig.documentation.text.Anchor;
 import org.flussig.documentation.text.Reader;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.FileSystem;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -41,43 +42,18 @@ import java.util.Set;
 public class DacDocCompile
     extends AbstractMojo
 {
-    @Parameter(readonly = true, defaultValue = "${project.build.sourceDirectory}")
+    @Parameter(readonly = true, defaultValue = "${project.build.directory}")
     private File srcDirectory;
 
     public void execute() throws MojoExecutionException
     {
         try {
-            File allSourceDir = srcDirectory.getParentFile().getParentFile().getParentFile();
+            File allSourceDir = srcDirectory;
 
-            getLog().info( String.format("Source directory: %s", allSourceDir.getAbsolutePath()));
+            getLog().info( String.format("Build directory: %s", allSourceDir.getAbsolutePath()));
 
             // prepare source directory: create resource folder with images for check results (if not exists)
-// TODO: uncomment
-//            File srcResourceDirectory = new File(getClass().getClassLoader().getResource("circle-green-12px.png").getFile()).getParentFile();
-//            getLog().info( String.format("Resource directory: %s", srcResourceDirectory.getAbsolutePath()));
-//
-//            File destResourceDirectory = Path.of(srcDirectory.getAbsolutePath(), Constants.RESOURCES).toFile().getParentFile();
-//            getLog().info( String.format("Dest resource directory: %s", destResourceDirectory.getAbsolutePath()));
-//
-//            if(!destResourceDirectory.exists()) {
-//                destResourceDirectory.mkdir();
-//                getLog().info( String.format("Dest resource directory created: %s", destResourceDirectory.getAbsolutePath()));
-//
-//            }
-//
-//            File destDacDocResourceDirectory = Path.of(destResourceDirectory.getAbsolutePath(), Constants.DACDOC_RESOURCES).toFile();
-//            getLog().info( String.format("DacDoc resource directory: %s", destDacDocResourceDirectory.getAbsolutePath()));
-//
-//
-//            if(!destDacDocResourceDirectory.exists()) {
-//                destDacDocResourceDirectory.mkdir();
-//                getLog().info( String.format("DacDoc resource directory created: %s", destDacDocResourceDirectory.getAbsolutePath()));
-//            }
-//
-//            // copy all files from source to dest
-//            Files.copy(srcResourceDirectory.toPath(), destDacDocResourceDirectory.toPath());
-//            getLog().info( String.format("Resources copied from source to dest: %s %s", srcResourceDirectory.getAbsolutePath(), destDacDocResourceDirectory.getAbsolutePath()));
-
+            prepareResourceDirectory(allSourceDir);
 
             // collect all readme files
             Set<File> readmeFiles = Reader.findMarkdownFiles(allSourceDir.toPath());
@@ -104,5 +80,35 @@ public class DacDocCompile
         } catch(Exception e) {
             throw new MojoExecutionException("exception while executing dacdoc-maven-plugin compile goal " + e.getMessage());
         }
+    }
+
+    // write necessary resources to dacdoc-resources directory
+    private void prepareResourceDirectory(File baseDir) throws IOException {
+        File destDacDocResourceDirectory = createDacDocResourceDir(baseDir);
+
+        List<String> indicatorFileNames = Arrays.asList(Constants.GREEN_IND, Constants.GREEN_IND, Constants.ORANGE_IND, Constants.RED_IND);
+
+        for(String indicatorFileName: indicatorFileNames) {
+            Path outPath = Path.of(destDacDocResourceDirectory.getAbsolutePath(), indicatorFileName);
+
+            try(InputStream stream = getClass().getClassLoader().getResource(indicatorFileName).openStream()) {
+                byte[] resourceBytes = stream.readAllBytes();
+                Files.write(outPath, resourceBytes);
+                getLog().info( String.format("resource file written: ", outPath));
+            } catch(Exception e) {
+                getLog().error(String.format("resource file failed: ", outPath), e);
+            }
+        }
+    }
+
+    private File createDacDocResourceDir(File baseDir) {
+        File destDacDocResourceDirectory = Path.of(baseDir.getAbsolutePath(), Constants.DACDOC_RESOURCES).toFile();
+        getLog().info( String.format("DacDoc resource directory: %s", destDacDocResourceDirectory.getAbsolutePath()));
+
+        if(!destDacDocResourceDirectory.exists()) {
+            destDacDocResourceDirectory.mkdir();
+            getLog().info( String.format("DacDoc resource directory created: %s", destDacDocResourceDirectory.getAbsolutePath()));
+        }
+        return destDacDocResourceDirectory;
     }
 }
